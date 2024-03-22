77ca3f8d4aff788d8a3b9fd2c535284cb6dcd477
Kays8 <thirayut1379@gmail.com>
2024-03-22 21:48:19 +0700

    Formik

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import SignupForm from './signupform'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+      <h1>Signup Form</h1>
+      <SignupForm/>
+    </>
+  )
+}
+
+export default App
